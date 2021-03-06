6001ebd39b3fdfb8e3a80b86038118ed2a1036a0
zahit <zahit_kaya58@hotmail.com>
2021-03-06 20:59:10 +0300

    fix

--- app/src/main/java/com/example/chat_app/ChatActivity.java
+++ app/src/main/java/com/example/chat_app/ChatActivity.java
@@ -1,5 +1,7 @@
 package com.example.chat_app;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,13 +9,20 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class ChatActivity extends AppCompatActivity {
@@ -22,7 +31,11 @@ public class ChatActivity extends AppCompatActivity {
     private EditText messageEditText;
     private FirebaseAuth mAuth;
     private FirebaseUser firebaseUser;
+    private ListView listView;
+    private ArrayList<String> messageList=new ArrayList<>();
+    private ArrayAdapter<String> adapter;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,10 +44,38 @@ public class ChatActivity extends AppCompatActivity {
         messageEditText=findViewById(R.id.messageEditText);
         databaseReference= FirebaseDatabase.getInstance().getReference("Messages");
         mAuth=FirebaseAuth.getInstance();
-      //  Log.d("email",receiverEmail);
+        listView=findViewById(R.id.messageListView);
+
+
 
         String receiverEmail=getIntent().getStringExtra("RECEIVER_EMAIL");
+        String receiverUid=getIntent().getStringExtra("RECEIVER_UID");
+        String senderUid=mAuth.getUid().toString();
+        String sortUid=sortUid(receiverUid,senderUid);
 
+
+        FirebaseDatabase.getInstance().getReference("Messages").child(sortUid).addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                messageList.clear();
+              for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    String message=dataSnapshot.getValue(String.class);
+                    messageList.add(message);
+                  adapter = new ArrayAdapter<String>(ChatActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, messageList);
+                  adapter.notifyDataSetChanged();
+                  listView.setAdapter(adapter);
+                }
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
     }
@@ -57,4 +98,5 @@ public class ChatActivity extends AppCompatActivity {
                 .toString();
         return sorted;
     }
+
 }
