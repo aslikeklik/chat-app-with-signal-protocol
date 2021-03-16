2974365ee62727e5f1ec3353c356c21801d35fc2
zahitkaya <zahit_kaya58@hotmail.com>
2021-03-16 21:13:24 +0300

    fragments package added

--- app/src/main/java/com/example/chat_app/MainActivity.java
+++ app/src/main/java/com/example/chat_app/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.chat_app;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -15,12 +12,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import com.example.chat_app.fragments.GirisFragment;
+import com.example.chat_app.fragments.KaydolFragment;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -29,8 +25,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.Date;
 
 import es.dmoral.toasty.Toasty;
 
--- app/src/main/java/com/example/chat_app/fragments/GirisFragment.java
+++ app/src/main/java/com/example/chat_app/fragments/GirisFragment.java
@@ -1,4 +1,4 @@
-package com.example.chat_app;
+package com.example.chat_app.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.example.chat_app.R;
 
 public class GirisFragment extends Fragment {
     @Override
