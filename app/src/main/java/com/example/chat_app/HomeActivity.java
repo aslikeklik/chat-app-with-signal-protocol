4f1f6d114a0938892c0d5e9a1720e709bce68423
asli <asli.keklik@bilgiedu.net>
2021-05-11 23:23:29 +0300

    deneme

--- app/src/main/java/com/example/chat_app/HomeActivity.java
+++ app/src/main/java/com/example/chat_app/HomeActivity.java
@@ -37,6 +37,7 @@ public class HomeActivity extends AppCompatActivity {
     private ArrayAdapter<String> adapter;
 
     String receiverUid;
+    //sdjkhfg
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
